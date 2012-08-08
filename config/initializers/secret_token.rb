ca74c871a41bdac95a79be8f8733c7933e984d2b
David C. Manuel, Jr <dmanuel@pflag.org>
2012-08-08 17:02:36 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Financials::Application.config.secret_token = '27782cea87a9bbaf1d0c087b76df77d157ed3002292e5a1fa3d670085a004954b2ce2a5a45e6c332a70577570f69ca4f54e654040fc5bf2059db377114e2f129'

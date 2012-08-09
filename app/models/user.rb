119e4a8caa0ecea50dd1aca2e92d836c0ff95fa5
David C. Manuel, Jr <dmanuel@pflag.org>
2012-08-09 11:37:24 -0400

    full_title helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email
   has_many :financial_statements
+  def initialize(attributes = {})
+    @first_name  = attributes[:first_name]
+    @last_name  = attributes[:last_name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@first_name} #{@last_name} <#{@email}>"
+  end
 end

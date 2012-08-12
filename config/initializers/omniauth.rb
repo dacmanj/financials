84ab1e3fbd536492b2477c51363d99cd7a1566a1
David Manuel <dmanuel@pflag.org>
2012-08-12 12:10:35 -0400

    openauth needs email username extract

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,11 @@
 OmniAuth.config.full_host = "http://localhost:3000"
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['pflag.org'], ENV['Nf6KwOvzHqR+SogKz8mfYdpw'], 
-            { :scope => 'https://www.googleapis.com/auth/docs https://www.googleapis.com/auth/userinfo.profile' }
-
-end
+  provider :google_oauth2, '862097400978.apps.googleusercontent.com', 'cr-0CszujuLY5xTpPisAYo2U', 
+            { :scope => 'https://www.googleapis.com/auth/docs https://www.googleapis.com/auth/userinfo.profile',
+              :access_type => 'online'}
+  
+end=begin
+	
+=end
